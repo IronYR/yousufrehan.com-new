469350779d9d33974c7e60f8b8ffb470f287f7d5
IronYR <yrehan67@gmail.com>
2021-03-18 21:50:04 +0300

    removed messing around in webpack

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
 const fs = require("fs");
 const blogPostsFolder = "./content";
-const getPathsForPosts = () => {
-    return fs
-        .readdirSync(blogPostsFolder)
-        .map(blogName => {
-            const trimmedName = blogName.substring(0, blogName.length - 3);
-            return {
-                [`/posts/${trimmedName}`]: {
-                    page: '/posts/[slug]',
-                    query: {
-                        slug: trimmedName,
-                    },
-                },
-            };
-        })
-        .reduce((acc, curr) => {
-            return { ...acc, ...curr };
-        }, {});
-}
+// const getPathsForPosts = () => {
+//     return fs
+//         .readdirSync(blogPostsFolder)
+//         .map(blogName => {
+//             const trimmedName = blogName.substring(0, blogName.length - 3);
+//             return {
+//                 [`/posts/${trimmedName}`]: {
+//                     page: '/posts/[slug]',
+//                     query: {
+//                         slug: trimmedName,
+//                     },
+//                 },
+//             };
+//         })
+//         .reduce((acc, curr) => {
+//             return { ...acc, ...curr };
+//         }, {});
+// }
 module.exports = {
     webpack: (cfg) => {
         cfg.module.rules.push(
@@ -29,10 +29,10 @@ module.exports = {
         )
         return cfg;
     },
-    async exportPathMap(defaultPathMap) {
-        return {
-            ...defaultPathMap,
-            ...getPathsForPosts(),
-        };
-    },
+    // async exportPathMap(defaultPathMap) {
+    //     return {
+    //         ...defaultPathMap,
+    //         ...getPathsForPosts(),
+    //     };
+    // },
 }
