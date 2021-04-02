0bce6a058415d3e529fe71d72e37dea594f61be2
IronYR <yrehan67@gmail.com>
2021-04-02 19:14:48 +0300

    changed favicon and added google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,22 @@ class MyDocument extends Document {
                     <link rel="icon" href="/favicon.ico" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap" rel="stylesheet"></link>
+                    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                    <link rel="manifest" href="/site.webmanifest" />
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                    <meta name="msapplication-TileColor" content="#da532c" />
+                    <meta name="theme-color" content="#ffffff"></meta>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HCG7XN2G47"></script>
+                    <script dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', 'G-HCG7XN2G47');
+                    `}} />
                 </Head>
                 <body>
                     <Main />
